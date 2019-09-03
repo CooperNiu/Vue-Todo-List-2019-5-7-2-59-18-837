e15436d1688fcd0ee2c9c6219349d908ac4ce352
CooperNiu <niuyanno.1@outlook.com>
2019-09-03 21:51:01 +0800

    refactor

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,26 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <todo-list/>
   </div>
 </template>
 
 <script>
+import TodoList from './components/TodoList.vue'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    TodoList
+  }
 }
 </script>
 
 <style>
+@import "./assets/css/styles.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
